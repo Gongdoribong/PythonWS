d585ffa3de71ef6b8bb0771468af24f70084dc43
Lumi <jiyoung2004120@gmail.com>
2023-01-03 16:00:29 +0900

    Create 문자열 나누기.py

---
+++ programmers/문자열 나누기.py
@@ -0,0 +1,27 @@
+def solution(s):
+    words=[]
+    result=[]
+
+    cnt_x = 0
+    cnt_notx = 0
+
+    s = list(s)
+    x = s[0]
+
+    for i in range(len(s)):
+        words.append(s[i])
+        if s[i] == x:
+            cnt_x += 1
+
+        else:
+            cnt_notx += 1
+
+        if cnt_x == cnt_notx or i == len(s)-1:
+            result.append(''.join(words))
+            words=[]    
+            cnt_x = 0
+            cnt_notx = 0
+            if i != len(s)-1:
+                x = s[i+1]
+
+    return len(result)
