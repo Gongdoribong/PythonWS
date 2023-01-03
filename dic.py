8d5f1ad651b5eeef6a2ac1597112abb2c4baa36d
Lumi <jiyoung2004120@gmail.com>
2023-01-03 15:58:55 +0900

    Update dic.py

--- dic.py
+++ dic.py
@@ -1,13 +1,18 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-x = np.arange(-3,4)
-y1 = -x+6
-y2 = x**2+1
+x = np.arange(6)
+month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+salePrice = [90, 88, 75, 55, 89, 95]
+ratio = [18.5, 19.5, 17.6, 22.3, 52.8, 19.5]
 
-plt.plot(x, y1, marker = 'o', linestyle = '--', color = 'b')
-plt.plot(x, y2, marker = 'x', linestyle = '--', color = 'r')
+plt.bar(x, salePrice)
+plt.xticks(x, month)
+plt.plot(x, ratio, marker = 'o', linestyle = '--', color = 'r')
 
-plt.grid(True, axis='y', linestyle = '--', color ='r')
+plt.title("Monthly Sales Report")
+plt.xlabel("Month")
+plt.ylabel("Sale amount")
+
 plt.show()
 
